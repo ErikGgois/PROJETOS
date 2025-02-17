f61c147881702ffba1d4a13ecdd66248ab87b3fd
Erik <erikgazetagois@hotmail.com>
2025-02-17 20:00:11 -0300

    Criando Botão de voltar

--- meu-projeto/src/pages/Cadastro.tsx
+++ meu-projeto/src/pages/Cadastro.tsx
@@ -1,12 +1,22 @@
+import { useNavigate } from "react-router-dom"
 import Cabecalho from "../Cabecalho"
 import Rodape from "../Rodape"
 
 function Cadastro() {
+
+    const navegacao = useNavigate();
+
+    function HandleVoltar(){
+        navegacao('/');
+
+    }
     return (
         <div>
             <Cabecalho />
             Faça seu cadastro
             <Rodape/>
+            <br /><br />
+            <button onClick={HandleVoltar} > VOLTAR </button>
         </div>
     )
 }
--- meu-projeto/src/pages/Login.tsx
+++ meu-projeto/src/pages/Login.tsx
@@ -1,12 +1,24 @@
+import { useNavigate, useParams } from "react-router-dom";
 import Cabecalho from "../Cabecalho"
 import Rodape from "../Rodape"
 
 function Login() {
+
+    
+    const QualquerCoisa = useParams();
+    const navegacao = useNavigate();
+
+    function HandleVoltar (){
+
+        navegacao('/');
+    }
     return (
         <div>
             <Cabecalho />
             Faça seu login
             <Rodape />
+            <br /><br />
+            <button onClick={HandleVoltar} > VOLTAR</button>
         </div>
     )
 }
